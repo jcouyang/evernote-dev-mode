44e4b685374b9aba2ad2faa9164be6e42fcf805a
Jichao Ouyang <oyanglulu@gmail.com>
2013-09-13 10:59:00 +0800

    use developer token instead of password

---
+++ evernote-mode-autoloads.el
@@ -0,0 +1,24 @@
+;;; evernote-mode-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
+
+;;;### (autoloads nil "evernote-mode-pkg" "evernote-mode-pkg.el"
+;;;;;;  (21042 29445 0 0))
+;;; Generated autoloads from evernote-mode-pkg.el
+ (require 'evernote-mode)
+ (setq evernote-enml-formatter-command '("w3m" "-dump" "-I" "UTF8" "-O" "UTF8"))
+
+;;;***
+
+;;;### (autoloads nil nil ("evernote-mode.el") (21042 29447 736668
+;;;;;;  0))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; evernote-mode-autoloads.el ends here
